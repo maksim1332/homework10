20ab29323360248bb9ba927d1099616679ef10a3
USER <mas9n9-mas9n91332@yandex.ru>
2024-10-08 05:18:21 +0500

    completed all the tasks!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //task 1
+        System.out.println("Task 1");
+        String firstName = "Ivanov ";
+        String middleName = "Ivan";
+        String lastName = " Ivanovich";
+        String fullName = firstName + middleName + lastName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+// task 2
+        System.out.println("Task 2");
+        String fullName1 = "Ivanov Ivan Ivanovich";
+        String vFullName=fullName.toUpperCase(Locale.ROOT);
+        System.out.println(vFullName);
+        //task 3
+        System.out.println("Task 3");
+        String fullName2 = "Иванов Семён Семёнович";
+        String zFullName = fullName2.replace('ё', 'е');
+        System.out.println(zFullName);
     }
 }
